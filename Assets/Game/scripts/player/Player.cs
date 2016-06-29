1651d15a88e4d9585ae8c20b1d244d58a3697952
Alex <A13x231mcsl@gmail.com>
2016-06-29 15:04:24 +0100

    Added Player script with controllers and motor.
    Also updated the test scenario.

---
+++ Assets/Game/scripts/player/Player.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    GameObject firstPersonCamPoint;
+
+    [SerializeField]
+    GameObject thirdPersonCamPoint;
+
+	// Use this for initialization
+	void Start () {
+        SwitchToThirdPerson();
+	}
+
+    void SwitchCameraMode()
+    {
+        if (GetComponent<ThirdPersonController>() != null)
+        {
+            SwitchToFirstPerson();
+        }
+        else
+        {
+            SwitchToThirdPerson();
+        }
+    }
+
+    void SwitchToThirdPerson ()
+    {
+        //Remove the First Person Controller if it's there.
+        Destroy(GetComponent<FirstPersonController>());
+        //Deactivate the first person camera.
+        firstPersonCamPoint.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+
+        //Add the Third Person Controller, setup variables.
+        this.gameObject.AddComponent<ThirdPersonController>();
+        ThirdPersonController _thirdPersonController = GetComponent<ThirdPersonController>();
+        _thirdPersonController.cameraPoint = thirdPersonCamPoint;
+    }
+
+    void SwitchToFirstPerson()
+    {
+        //Remove the Third Person Controller if it's there.
+        Destroy(GetComponent<ThirdPersonController>());
+        //Deactivate the third person camera.
+        thirdPersonCamPoint.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+
+        //Add the First Person Controller, setup variables.
+        this.gameObject.AddComponent<FirstPersonController>();
+        FirstPersonController _firstPersonController = GetComponent<FirstPersonController>();
+        _firstPersonController.cameraPoint = firstPersonCamPoint;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            SwitchCameraMode();
+        }
+
+	}
+}
---
+++ Assets/Game/scripts/player/ThirdPersonController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonController : MonoBehaviour {
+
+    [SerializeField]
+    public GameObject cameraPoint;
+
+    public void UpdateCamPoint (GameObject _camPoint)
+    {
+        cameraPoint = _camPoint;
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        //Wait for campoint available.
+	    while(cameraPoint == null)
+        {
+
+        }
+
+        //Activate camera.
+        cameraPoint.gameObject.transform.GetChild(0).gameObject.SetActive(true);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
