f03887c454b1bd39d5b489883ec53056107396c5
Alex <A13x231mcsl@gmail.com>
2016-06-30 02:49:32 +0100

    Added some notes on jump types.

---
+++ Assets/Game/scripts/player/PlayerOptions.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerOptions : MonoBehaviour {
+
+    abstract public class JumpTypes
+    {
+        public string jumpName;
+        public float jumpDuration;
+        public int jumpCount;
+        abstract public void Jump();
+    }
+
+    public class StandardJump : JumpTypes
+    {
+        public string jumpName = "Standard";
+        public float jumpDuration = 1;
+        public int jumpCount = 3;
+        public override void Jump()
+        {
+            //initial jump, wait duration, extra jumps until count over.
+        }
+    }
+
+    public class GlideJump : JumpTypes
+    {
+        public string jumpName = "Glide";
+        public float jumpDuration = 3;
+        public int jumpCount = 3;
+        public override void Jump()
+        {
+            //initial jump, extra jumps glide in facing direction.
+        }
+    }
+
+    public class LiftJump : JumpTypes
+    {
+        public string jumpName = "Lift";
+        public float jumpDuration = 5;
+        public int jumpCount = 2;
+        public override void Jump()
+        {
+            //initial jump, second jump slowly glides upwards.
+        }
+    }
+
+    public class WarpJump : JumpTypes
+    {
+        public string jumpName = "Warp";
+        public float jumpDuration = 1;
+        public int jumpCount = 3;
+        public override void Jump()
+        {
+            //initial jump, next 2 jumps warp the player to facing direction.
+        }
+    }
+}
