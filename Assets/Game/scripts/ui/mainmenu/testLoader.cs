e488a372ef22ad24a4078fdfd01fcce7e8c7f8cc
Alex-231 <git+a13x231mcsl@gmal.com>
2016-07-09 17:42:01 +0100

    WIP new scene loader (sync)

--- Assets/Game/scripts/ui/mainmenu/testLoader.cs
+++ Assets/Game/scripts/ui/mainmenu/testLoader.cs
@@ -14,5 +14,14 @@ public class testLoader : MonoBehaviour {
         GUI.Box(containerBoxRect, "Load a Scenario");
         if(GUI.Button(button1Rect, "PlayerControllerTest"))
             SceneManager.LoadScene("PlayerControllerTest");
+
+        //for(int i = 0; i <= SceneManager.sceneCountInBuildSettings; i++)
+        //{
+        //    string _sceneName = SceneManager.GetSceneAt(i).name;
+        //    Rect _buttonRect = new Rect((Screen.width / 2) - 70, (Screen.height / 2) - 70, 140, 20);
+
+        //    if (GUI.Button(button1Rect, _sceneName))
+        //        SceneManager.LoadScene(i);
+        //}
     }
 }
